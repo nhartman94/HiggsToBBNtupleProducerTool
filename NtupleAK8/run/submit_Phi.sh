046e6ff0faad2ae518126b248b6e068a59557337
cvernier <caterina.vernieri@cern.ch>
2017-12-04 14:35:47 +0100

    run Phi

---
+++ NtupleAK8/run/submit_Phi.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_1000_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_1000_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_100_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_100_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_10_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_10_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_125_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_125_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_150_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_200_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_200_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_20_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_20_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_300_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_300_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_350_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_350_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_400_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_400_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_500_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_500_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_50_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_50_Scalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_750_PseudoScalar.py
+echo '======'
+echo
+
+crab submit -c jobs/crab_submit_Spin0_ggPhibb1j_g1_750_Scalar.py
+echo '======'
+echo
+
+
+echo 'Done!'
